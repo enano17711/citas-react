5bac9a09aca37cead72f3a3091354cb7acd4dc77
enano17711 <enano17711@gmail.com>
2023-02-13 08:26:02 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+    return (
+        <div className="App">
+            hola mundo
+            <p>esto es un parrafo en react</p>
+        </div>
+    )
+}
+
+export default App
